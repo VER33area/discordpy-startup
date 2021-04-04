92a628d77a884feb6a816491c6487183d5069946
VER33area <77556652+VER33area@users.noreply.github.com>
2021-04-04 17:51:53 +0900

    implement zone

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,11 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+    
+@bot.command()
+async def よお(ctx):
+    await ctx.send('こんにちは')
 
 
 bot.run(token)
